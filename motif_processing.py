24f4709a8ba52e05805a50378430fd296e128ac3
PK <pganga@tf2.lisanwanglab.org>
2024-04-03 21:52:24 +0000

    motif proessing

---
+++ motif_processing.py
@@ -0,0 +1,120 @@
+import os
+import urllib.request
+import gzip
+import subprocess
+
+# Folders
+motif_file = "motif"
+peaks_folder = "peaks/ByCellType/TF/test"
+
+# Function to check if a file exists
+def file_exists(file_path):
+    return os.path.isfile(file_path)
+
+# File download
+def download_file(url, folder):
+    filename = os.path.basename(url)
+    file_path = os.path.join(folder, filename)
+
+    if file_exists(file_path):
+        print(f"File {filename} already exists in {folder}. Skipping download.")
+    else:
+        try:
+            urllib.request.urlretrieve(url, file_path)
+        except Exception as e:
+            print(f"Error downloading {url}: {e}")
+            exit(1)
+
+# Process motif file
+def process_motiffile(input_file, output_file):
+    with gzip.open(input_file, 'rt') as f_in, open(output_file, 'w') as f_out:
+        for line in f_in:
+            fields = line.strip().split()
+            f_out.write('\t'.join(fields[1:8] + [fields[0]]) + '\n')
+
+    print(f"Motif positions are written to {output_file}")
+
+# Function to get reference sequence
+def get_reference(sequenceID):
+    fa = "hg19.fa"
+    output = subprocess.check_output(["samtools", "faidx", fa, sequenceID]).decode("utf-8")
+    lines = output.split('\n')
+    ref_sequence = ''.join(lines[1:])
+    return ref_sequence.upper()
+
+# Function to get reverse complement sequence
+def get_reverse_complement(sequence):
+    complement = {'a': 't', 'c': 'g', 'g': 'c', 't': 'a'}
+    reverse_complement = ''
+    sequence = sequence.lower()
+    for base in reversed(sequence):
+        if base in complement:
+            reverse_complement += complement[base]
+        else:
+            reverse_complement += base
+    return reverse_complement.upper()
+
+
+# Function to process bed file
+def add_reference(input_file, output_file):
+    with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
+
+        # Define the header
+        header = "#chrom\tchromStart\tchromEnd\tname\tscore\tstrand\tnum\tbinding_sequence"
+
+        # Write the header to the output file
+        f_out.write(header + '\n')
+
+        for line in f_in:
+            fields = line.strip().split('\t')
+            chromosome = fields[0]
+            start = fields[1]
+            end = fields[2]
+            strand = fields[5]
+            sequenceID = f"{chromosome}:{start}-{end}"
+            
+            if strand == '+':
+                hg19_sequence = get_reference(sequenceID)
+            elif strand == '-':
+                ref_sequence = get_reference(sequenceID)
+                hg19_sequence = get_reverse_complement(ref_sequence)
+            else:
+                print("Invalid strand information.")
+                continue
+            
+            # Append the hg19_sequence to the fields
+            fields.append(hg19_sequence)
+            
+            # Write the modified line to the output file
+            f_out.write('\t'.join(fields) + '\n')
+
+    print(f"Processed data written to {output_file}")
+
+    bgzip_cmd = f"bgzip -f {output_file}"
+    subprocess.run(bgzip_cmd, shell=True)
+
+    print(f"Compressed data written to {output_file}.gz")
+
+# Create directory if it doesn't exist
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+# Define an array of folder names
+folders = [motif_file]
+
+# Loop through each folder name
+for folder in folders:
+    # Check if the directory exists
+    create_directory(folder)
+
+# Download experiment table 
+download_file("http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/factorbookMotifPos.txt.gz", motif_file)
+
+# Process the downloaded file
+input_file_path = os.path.join(motif_file, "factorbookMotifPos.txt.gz")
+output_file_path = os.path.join(motif_file, "factorbookMotifPos.bed")
+process_motiffile(input_file_path, output_file_path)
+
+output_path = os.path.join(motif_file, "factorbookMotifPosWithSequence.bed" )
+add_reference(output_file_path, output_path)
